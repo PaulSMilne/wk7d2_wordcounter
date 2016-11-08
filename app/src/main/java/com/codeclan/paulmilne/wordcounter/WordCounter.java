fc0ba6b4f7e1f38228212cbca840ebb5f16d32f8
Paul Milne <hallhill@gmail.com>
2016-11-08 20:38:56 +0000

    sorted out nested methods on WordCounter class page and fixed any obvious redline errors.

--- app/src/main/java/com/codeclan/paulmilne/wordcounter/WordCounter.java
+++ app/src/main/java/com/codeclan/paulmilne/wordcounter/WordCounter.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
  * Created by user on 08/11/2016.
  */
 
-public class WordCounter extends AppCompatActivity {
+public class WordCounter extends AppCompatActivity { //start WordCounter class statement
 
     EditText mSentenceEditText;
     Button mCountButton;
@@ -22,7 +22,7 @@ public class WordCounter extends AppCompatActivity {
     TextView mAnswerText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState){ // start onCreate method
         Log.d("WordCounter", "onCreate called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -31,16 +31,24 @@ public class WordCounter extends AppCompatActivity {
 
         mCountButton = (Button) findViewById(R.id.count_words);
 
-        mCountButton.setOnClickListener(new View.OnClickListener() {
-            String sentence = mSentenceEditText.getText().toString();
+        mCountButton.setOnClickListener(new View.OnClickListener() { //open brackets, open curley brackets
+            @Override
+            public void onClick(View view) { //open OnClick(View view)
+                Log.d("WordCounter", "Count button clicked!");
+                String sentence = mSentenceEditText.getText().toString();
+                Log.d("EightBall", "The sentence entered was '" + mSentence + "'");
+                mSentence = new CountWords(sentence);
+                int wordCount = mSentence.countWords();
 
-            int wordCount = mSentence.countWords();
+                mAnswerText = (TextView) findViewById(R.id.answer_text);
 
-            mAnswerText = (TextView) findViewById(R.id.);
+                int answer = wordCount;
 
+                mAnswerText.setText(answer);
+            } //close onclick(View view)
 
+        }); //close curly brackets, close brackets
 
-        });
-    }
+    } //end onCreate method
 
-}
+} //end WordCounter class statement
